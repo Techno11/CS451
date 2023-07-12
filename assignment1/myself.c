ab35ac2f69f371c07a2d8798212e76e806fac46a
Soren Zaiser <soren.zaiser@essiumlabs.com>
2023-07-12 16:27:25 -0400

    More comments

--- assignment1/myself.c
+++ assignment1/myself.c
@@ -3,23 +3,27 @@
 #include <stdio.h>    // printf 
 #include <string.h>   // strcat
 
+// Check to see if a file exists
+// @param filename The name of the file to check
 bool file_exists (char *filename) {
   struct stat   buffer;   
   return (stat (filename, &buffer) == 0);
 }
 
+// Convert an integer to a string
+// @param val The integer to convert
+// @param base The base to convert to (radix)
 char* itoa(int val, int base){
-	
+    // Create a static buffer
 	static char buf[32] = {0};
-	
+    // Initialize the index
 	int i = 30;
-	
-	for(; val && i ; --i, val /= base)
-	
+    // Convert the integer to a string
+	for(; val && i ; --i, val /= base) {
 		buf[i] = "0123456789abcdef"[val % base];
-	
+    }
+	// Do some fancy pointer arithmetic to return the string
 	return &buf[i+1];
-	
 }
 
 // Main method
@@ -37,10 +41,13 @@ int main(void) {
             // Print that it exists
             printf("Process %d exists\n", i);
             processesFound++;
+            // TODO: Read and print the processes statistics
         }
     }
 
+    // Print total # processes found
     printf("Found %d processes\n", processesFound);
 
+    // Exit
     return 0;
 }
