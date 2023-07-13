77bea39c29b6349b2edb43e3b5717d8b807a7ff7
Soren Zaiser <soren.zaiser@essiumlabs.com>
2023-07-13 11:16:59 -0400

    Format

--- assignment1/mypself.c
+++ assignment1/mypself.c
@@ -1,28 +1,28 @@
 //
 // Created by bsting on 7/12/23.
 //
-#include <sys/stat.h>   // stat (import struct def. of stat)
-#include <stdbool.h>    // bool type
+#include <sys/stat.h> // stat (import struct def. of stat)
+#include <stdbool.h>  // bool type
 #include <stdio.h>    // printf
-#include <stdlib.h> //for alloc functions
+#include <stdlib.h>   //for alloc functions
 #include <string.h>   // strcat
 #include <time.h>
 #include <pwd.h>
-#include <unistd.h>   // sysconf
+#include <unistd.h> // sysconf
 
-//Finds the number of characters on a line in a file:
+// Finds the number of characters on a line in a file:
 //(use this with fgets() to properly declare max char size param)
-int amtOfCharOnLine(char* filepath, int lineNum)
+int amtOfCharOnLine(char *filepath, int lineNum)
 {
     int totalChars = 0;
     int currLineNum = 0;
     char currentChar;
 
-    FILE* file = fopen(filepath, "r");
+    FILE *file = fopen(filepath, "r");
     currentChar = getc(file);
 
     // First, locate correct line to count on
-    while (currentChar!= EOF)
+    while (currentChar != EOF)
     {
         if (currentChar == '\n')
         {
@@ -35,7 +35,7 @@ int amtOfCharOnLine(char* filepath, int lineNum)
         currentChar = getc(file);
     }
 
-    //Then count each character on correct line until next line or EOF is encountered:
+    // Then count each character on correct line until next line or EOF is encountered:
     while ((currentChar != EOF) && (currentChar != '\n'))
     {
         currentChar = getc(file);
@@ -45,18 +45,18 @@ int amtOfCharOnLine(char* filepath, int lineNum)
     return totalChars;
 }
 
-//Finds the total lines in a file
+// Finds the total lines in a file
 int findFileLines(char *filepath)
 {
-    //Initialize values for total file line count and character tracker
+    // Initialize values for total file line count and character tracker
     int totalLines = 0;
     char currentChar;
 
-    //Begin with opening file and getting the first character
-    FILE * file = fopen(filepath, "r");
+    // Begin with opening file and getting the first character
+    FILE *file = fopen(filepath, "r");
     currentChar = getc(file);
 
-    //While we haven't reached the end of file, count the # of newlines
+    // While we haven't reached the end of file, count the # of newlines
     while (currentChar != EOF)
     {
         if (currentChar == '\n')
@@ -68,21 +68,21 @@ int findFileLines(char *filepath)
 
     fclose(file);
 
-    return totalLines + 1; //Return the total number of file lines
+    return totalLines + 1; // Return the total number of file lines
     //(e.g. 0 1 2 = ...\n...\n...EOF = total lines are 3
 }
 
 // Get the number of digits in an integer (useful for int-to-str)
-int countIntDigits (long passedInt)
+int countIntDigits(long passedInt)
 {
-    //Filter out single digits:
+    // Filter out single digits:
     passedInt = passedInt / 10;
-    int count = 1; //digit tracker (even 0 has 1 digit)
+    int count = 1; // digit tracker (even 0 has 1 digit)
 
-    while (passedInt != 0) //while there's still digits left (no cutoff)
+    while (passedInt != 0) // while there's still digits left (no cutoff)
     {
-        passedInt = passedInt / 10; //cut off next decimal place
-        count++; //we moved over, so that's a digit
+        passedInt = passedInt / 10; // cut off next decimal place
+        count++;                    // we moved over, so that's a digit
     }
     return count;
 }
@@ -90,11 +90,12 @@ int countIntDigits (long passedInt)
 // Build a path to a file in the /proc directory
 // @param pid The process ID
 // @param path The path to the file
-char *build_path(int pid, char* path) {
+char *build_path(int pid, char *path)
+{
     // Allocate more than enough space for the path
-    char* str = malloc(strlen("/proc/") + strlen(path) + 1 + 5); // 5 for "/proc/", 1 for '\0'
-    sprintf(str, "/proc/%d/%s", pid, path); // write the formatted string to buffer pointed by str
-    return str; // return the fully allocated and fully formatted/written path
+    char *str = malloc(strlen("/proc/") + strlen(path) + 1 + 5); // 5 for "/proc/", 1 for '\0'
+    sprintf(str, "/proc/%d/%s", pid, path);                      // write the formatted string to buffer pointed by str
+    return str;                                                  // return the fully allocated and fully formatted/written path
 }
 
 // Get process TIME value
@@ -140,45 +141,48 @@ char *get_proc_time(int pid)
 
 // Check to see if a file exists
 // @param filename The name of the file to check
-bool file_exists (char *filepath) {
-    struct stat buffer; //reference to stat's buffer (will fill next)
-    return (stat (filepath, &buffer) == 0); // returns 0 on stat success, else -1 + err
+bool file_exists(char *filepath)
+{
+    struct stat buffer;                    // reference to stat's buffer (will fill next)
+    return (stat(filepath, &buffer) == 0); // returns 0 on stat success, else -1 + err
 }
 
 // Main method
-int main(void) {
+int main(void)
+{
     int processesFound = 0;
 
     // Print the header
     printf("%-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s\n", "PID", "UID", "PPID", "LWP", "NLWP", "STIME", "STAT", "TIME", "CMD");
 
     // Iterate over all possible process IDs (PIDs)
-    for(int i = 0; i < 32768; i++) {
+    for (int i = 0; i < 32768; i++)
+    {
         // Build the path to the PID folder
         char *path = build_path(i, "");
 
         // Check to see if the file exists
-        if(file_exists(path))
+        if (file_exists(path))
         {
             // IT'S A VALID PID:
 
             // Get status file inside PID folder; it has good info we need
-            char* statusFile = build_path(i, "status");
+            char *statusFile = build_path(i, "status");
             int numOfLines = findFileLines(statusFile);
-            FILE* statusPtr = fopen(statusFile, "r");
+            FILE *statusPtr = fopen(statusFile, "r");
 
             struct stat st;
             stat(path, &st);
 
             // Process UID
             struct passwd *pwd = getpwuid(st.st_uid);
-            char* UID = pwd->pw_name;
+            char *UID = pwd->pw_name;
 
             // Process PID
             int numLength = countIntDigits(i);
-            //char pid[numLength + 1 + 1]; // +1 for (-) and +1 for '\0'
-            char* strPid;
-            strPid = (char*)malloc((sizeof(char) * numLength) + 1 + 1); // +1 for (-) and +1 for '\0'
+            // char pid[numLength + 1 + 1]; // +1 for (-) and +1 for '\0'
+            char *strPid;
+            strPid = (char *)malloc((sizeof(char) * numLength) + 1 + 1); // +1 for (-) and +1 for '\0'
             sprintf(strPid, "%d", i);
 
             // Process PPID
@@ -189,8 +193,8 @@ int main(void) {
             for (int lineIndex = 0; lineIndex < numOfLines; lineIndex++)
             {
                 charsOnLine = amtOfCharOnLine(statusFile, lineIndex);
-                char* fileLine;
-                fileLine = (char*)malloc((sizeof(char)*charsOnLine) + 1 + 1); //+1 for '\0' and + 1 for '\n' in case
+                char *fileLine;
+                fileLine = (char *)malloc((sizeof(char) * charsOnLine) + 1 + 1); //+1 for '\0' and + 1 for '\n' in case
                 fgets(fileLine, charsOnLine, statusPtr);
                 // TODO: Check for the PPid!
             }
@@ -200,7 +204,7 @@ int main(void) {
             // Process NLWP
 
             // Process STIME
-            char* STIME = malloc(5);
+            char *STIME = malloc(5);
             strftime(STIME, 100, "%H:%M", localtime(&st.st_atime));
 
             // Process STAT
