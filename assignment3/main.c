30b2a30ba84e8c8fc8d8e345273da52d5280bf00
Brendan Sting <67481528+Bsting118@users.noreply.github.com>
2023-08-23 16:45:43 -0400

    Add files via upload

---
+++ assignment3/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void thread_task(int i)
+{
+    // ...
+    printf("A thread has been made!\n");
+    pthread_exit(0); // this code returns to the corresponding pthread_join issue in main()
+}
+
+int main() {
+    printf("Hello, World!\n");
+
+    pthread_t thread_id[5];
+
+    // The following code creates 5 threads:
+    for (int i=0; i<5; i++)
+    {
+        // Will create a thread:
+        pthread_create(&thread_id[i], NULL, thread_task,  (void*)i);
+    }
+
+    //The following code makes sure the main program waits until all threads have finished execution
+    for (int i=0; i<5;i++)
+    {
+        pthread_join(thread_id[i], NULL);
+    }
+
+    return 0;
+}
